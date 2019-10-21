9231d38ec19fac88d87371b7e9bf95e5ba57b24d
yeongjet <625436194@163.com>
2019-10-21 11:58:51 +0800

    update

---
+++ packages/validator/src/optional.ts
@@ -0,0 +1,11 @@
+import * as t from 'io-ts'
+
+export const optional = <T extends t.Any>(
+    type: T,
+    name = `${type.name} | undefined`
+): t.UnionType<
+    [T, t.UndefinedType],
+    t.TypeOf<T> | undefined,
+    t.OutputOf<T> | undefined,
+    t.InputOf<T> | undefined
+> => t.union<[T, t.UndefinedType]>([type, t.undefined], name)
