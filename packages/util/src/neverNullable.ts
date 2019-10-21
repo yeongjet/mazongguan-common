60ec11d7f7f9df143388d6e6dbb579142f31d909
yeongjet <625436194@163.com>
2019-10-21 10:53:23 +0800

    update

--- packages/util/src/neverNullable.ts
+++ packages/util/src/neverNullable.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
 import { throwError, of } from 'rxjs';
 
+
 export const neverNullable = <T>(data: T) => 
     _.isNil(data)? throwError(new Error()) : of(data as NonNullable<T>)
