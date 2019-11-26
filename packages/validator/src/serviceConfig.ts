fa87acf7ad91c17707d2457bb8a457e6f9dd0582
yeongjet <625436194@163.com>
2019-11-26 22:11:49 +0800

    update

---
+++ packages/validator/src/serviceConfig.ts
@@ -0,0 +1,16 @@
+import * as Joi from 'joi'
+
+export const serviceConfig = {
+    redis: Joi.object({
+        host: Joi.string().min(1).required(),
+        port: Joi.number().integer().min(1025).max(65534).required(),
+    }),
+    wechat: Joi.object({
+        component: Joi.object({
+            appid: Joi.string().min(1).required(),
+            app_secret: Joi.string().min(1).required(),
+            token: Joi.string().min(1).required(),
+            encoding_aes_key: Joi.string().min(1).required()
+        })
+    })
+}
